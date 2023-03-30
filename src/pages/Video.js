e02c3e240291ff303fa89c6d25c4a94d3f1cdc41
isabelirk <isabelireik2@gmail.com>
2023-03-29 23:32:38 -0300

    passo 2

---
+++ src/pages/Video.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+//rcfe - novo código base do react
+
+function Video() {
+  const videoRef = useRef(null); //ação para dar play no vídeo
+  const [play, setPlay] = useState(false); //ação para pausar o vídeo
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        //controls
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
